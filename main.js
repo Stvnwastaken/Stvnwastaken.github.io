260e15492dd209ad7253d4f73d0a47c8c0a9fef5
LightLord <stevensah88@gmail.com>
2024-01-19 12:07:11 -0800

    fix: github link

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.querySelector("#app").innerHTML = `
     <h1>Hello World!</h1>
     <h3>I'm Steven, this is a temporary site while I learn other stuff. I will probably come back to this when I think I actually need a portfolio website.</h3>
     <div class="card">
-      <a class="button "href="https://github.com/LightLordYT" target="_blank">Click me</a>
+      <a class="button "href="https://github.com/Stvnwastaken" target="_blank">Click me</a>
     </div>
     <p class="read-the-docs">
       Click on the cool button to see my github
