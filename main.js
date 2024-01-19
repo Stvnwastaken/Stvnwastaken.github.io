f294c3da0ba937430cdd1a114a409816fa260863
LightLord <stevensah88@gmail.com>
2024-01-19 11:15:12 -0800

    temp intial site

---
+++ main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hello World!</h1>
+    <h3>I'm Steven, this is a temporary site while I learn other stuff. I will probably come back to this when I think I actually need a portfolio website.</h3>
+    <div class="card">
+      <a class="button "href="https://github.com/LightLordYT" target="_blank">Click me</a>
+    </div>
+    <p class="read-the-docs">
+      Click on the cool button to see my github
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
